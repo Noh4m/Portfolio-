92295750bf3b4cf9506dd13f2b77bb147f160dbf
Noham <72859612+Noh4m@users.noreply.github.com>
2022-05-03 00:17:26 +0200

    fix: sideBar

--- src/components/TextWithBar.js
+++ src/components/TextWithBar.js
@@ -3,7 +3,7 @@ function TextWithBar(props) {
     return (
         <div className="md:pt-16 md:pl-20 pt-6 pl-6">
             <h2 className="text-1xl font-bold md:text-2xl">Portfolio .</h2>
-            <p className="text-1xl flex flex-col md:text-2xl">2022 <span className=" mt-4 ml-6 border-solid border-white h-52 inline-block border-l-2"></span></p>
+            <p className="text-1xl flex flex-col md:text-2xl">2022 <span className="mt-4 ml-4 border-solid border-white h-52 inline-block border-l-2 md:mt-4 md:ml-6 "></span></p>
         </div>
     );
 }
