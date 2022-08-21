3b6cdf362c5fa715c3939df27bc4fa96d31fe27f
HIREP Noham <n.hirep@DIGI-LAPA141.local>
2022-08-21 13:29:14 +0200

    add new components

--- src/components/ArrowScroll.jsx
+++ src/components/ArrowScroll.jsx
@@ -5,9 +5,9 @@ import React from 'react';
 function ArrowScroll(props) {
     return (
         <div className="arrow absolute left-1/2 bottom-16">
-            <span></span>
-            <span></span>
-            <span></span>
+            <span className={props.color}></span>
+            <span className={props.color}></span>
+            <span className={props.color}></span>
         </div>
     );
 }
---
+++ src/components/Cover.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import IconAndPresentation from "../components/IconAndPresentation";
+import TextWithBar from "../components/TextWithBar";
+import ArrowScroll from "../components/ArrowScroll";
+import data from "../data/Data";
+
+function Cover(props) {
+    return (
+        <div className="page1 bg-black relative h-screen text-white flex font-sans ">
+                <TextWithBar title="Portfolio ." date="2022"/>
+                <div className=" absolute top-1/2 left-1/2 -translate-y-3/4  -translate-x-2/4  laptop:left-1/2  laptop:mx-auto">
+                    <IconAndPresentation firstName={data.firstName} work={data.work}
+                                         hearderSubTitle={data.hearderSubTitle}/>
+                </div>
+                <ArrowScroll color="borderWhite" />
+            </div>
+    );
+}
+
+export default Cover;
